5a990214af9489387948871161af454b761c1f28
Alexios Ivannicoff <ivannicoff@outlook.com>
2025-08-09 19:15:12 +0300

    chore: add Excelsior UI Demo application stub
    - Added a placeholder demo project for future implementation.
    - Changes to this project may occur as new features are introduced.

---
+++ ExcelsiorUIDemo/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+/*
+    Excelsior UI Framework
+    © 2025 Alexios Ivannicoff. All rights reserved.
+    Esse quam videri — To be rather than to seem.
+
+    This source code is distributed under the terms of the MIT License,
+    full text if not included: https://opensource.org/licenses/MIT
+*/
+
+using System.Windows;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
---
+++ ExcelsiorUIDemo/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Excelsior UI Framework
+    © 2025 Alexios Ivannicoff. All rights reserved.
+    Esse quam videri — To be rather than to seem.
+
+    This source code is distributed under the terms of the MIT License,
+    full text if not included: https://opensource.org/licenses/MIT
+*/
+
+using System.Windows;
+
+namespace ExcelsiorUIDemo;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
